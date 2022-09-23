af30af5574a7abd873e41bf4ccf881caf4495d64
Roberttamaia <robertta.94@gmail.com>
2022-09-23 15:49:37 +0000

    changing tab-routing

--- mobdev-ionic-intro/src/app/app-routing.module.ts
+++ mobdev-ionic-intro/src/app/app-routing.module.ts
@@ -3,51 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
-];	
-const routes: Routes = [
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-        path: 'films',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('../films/films.module').then( m => m.FilmsPageModule)
-          },
-          {
-            path: ':id',
-            loadChildren: () => import('../film-details/film-details.module').then( m => m.FilmDetailsPageModule)
-          }
-        ]
-      },
-      {
-        path: 'people',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('../people/people.module').then( m => m.PeoplePageModule)
-          }
-        ]
-      },
-      {
-        path: 'planets',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('../planets/planets.module').then( m => m.PlanetsPageModule)
-          }
-        ]
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/films',
-    pathMatch: 'full'
-  }
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
