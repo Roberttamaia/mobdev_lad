af035dd8bde9e751487532fb326be99cf6f45c87
Roberttamaia <robertta.94@gmail.com>
2022-11-04 13:58:40 +0000

    people upgrade

--- mobdev-ionic-intro/src/app/pages/people/people.page.ts
+++ mobdev-ionic-intro/src/app/pages/people/people.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-people',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeoplePage implements OnInit {
 
-  constructor() { }
+  people: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.people = this.http.get('https://swapi.dev/api/people');
+  }
+ 
+  openDetails(people) {
+    let split = people.url.split('/');
+    let peopleID = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/people/${peopleID}`);
   }
 
 }
